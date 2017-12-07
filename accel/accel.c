26380fbc69b4deedf0628748f38461b2b3744bfe
John Stebbins <stebbins@jetheaddev.com>
2017-12-07 11:23:12 -0800

    xxx

--- accel/accel.c
+++ accel/accel.c
@@ -9,12 +9,9 @@
 #include <linux/fs.h>
 #include <linux/uaccess.h>
 #include <linux/miscdevice.h>
-#include "16c750_support.h"
+#include "lis331dlh_regs.h"
 
 #define DRV_VERSION "0.01"
-
-#define UART2_BASE 0x48024000
-#define UART2_LEN  0x1000
 
 struct accel_device_data {
 };
@@ -26,7 +23,7 @@ static ssize_t accel_x_show(struct device *dev,
     u16 value;
     int loop;
 
-    snprintf(buf, PAGE_SIZE, TODO);
+    snprintf(buf, PAGE_SIZE, "TODO");
     return strlen(buf) + 1;
 }
 
@@ -36,7 +33,7 @@ static ssize_t accel_y_show(struct device *dev,
     u16 value;
     int loop;
 
-    snprintf(buf, PAGE_SIZE, TODO);
+    snprintf(buf, PAGE_SIZE, "TODO");
     return strlen(buf) + 1;
 }
 
@@ -46,7 +43,7 @@ static ssize_t accel_z_show(struct device *dev,
     u16 value;
     int loop;
 
-    snprintf(buf, PAGE_SIZE, TODO);
+    snprintf(buf, PAGE_SIZE, "TODO");
     return strlen(buf) + 1;
 }
 
@@ -56,8 +53,12 @@ static ssize_t accel_rate_show(struct device *dev,
     u16 value;
     int loop;
 
-    snprintf(buf, PAGE_SIZE, TODO);
+    snprintf(buf, PAGE_SIZE, "TODO");
     return strlen(buf) + 1;
+}
+
+static void set_rate(int rate)
+{
 }
 
 static ssize_t accel_rate_store(struct device *dev,
@@ -65,7 +66,7 @@ static ssize_t accel_rate_store(struct device *dev,
                                 const char *buf, size_t count)
 {
     printk("store got (%s)\n", buf);
-    set_rate();
+    set_rate(0);
     return count;
 }
 
@@ -73,23 +74,23 @@ static ssize_t accel_rate_store(struct device *dev,
 static DEVICE_ATTR(x, 0444, accel_x_show, NULL);
 static DEVICE_ATTR(y, 0444, accel_y_show, NULL);
 static DEVICE_ATTR(z, 0444, accel_z_show, NULL);
-static DEVICE_ATTR(z, 0644, accel_rate_show, accel_rate_stroe);
+static DEVICE_ATTR(rate, 0644, accel_rate_show, accel_rate_store);
 
 // Do hardware init
 static int accel_init(struct i2c_client * client,
                       struct accel_device_data *add)
 {
     int ret;
+    u32 who_am_i;
 
-
+    who_am_i = i2c_smbus_read_byte_data(client, LIS331DLH_WHO_AM_I_REG);
+    printk("Who Am I %x\n", who_am_i);
     return 0;
 }
 
 static void accel_deinit(struct i2c_client *client,
                          struct accel_device_data *add)
 {
-    int ret;
-
 }
 
 static __devinit int accel_dev_probe(struct i2c_client *client,
@@ -97,9 +98,6 @@ static __devinit int accel_dev_probe(struct i2c_client *client,
 {
     int ret;
     struct accel_device_data *add;
-    struct i2c_adapter * adapter;
-    struct i2c_client  * client;
-    struct i2c_board_info info = {};
 
     printk("accel_dev_probe\n");
     add = kmalloc(sizeof(struct accel_device_data), GFP_KERNEL);
@@ -109,24 +107,6 @@ static __devinit int accel_dev_probe(struct i2c_client *client,
     }
     memset(add, 0, sizeof(struct accel_device_data));
 
-    adapter = i2c_get_adapter(1);
-    if (adapter == NULL) {
-        printk("i2c_get_adaptter failed\n");
-        return -ENODEV;
-    }
-
-    strlcpy(info.type, "accelerometer", I2C_NAME_SIZE);
-    info.addr = TODO;
-    info.irq = TODO;
-    info.of_node = TODO;
-    info.archdata = TODO;
-    info.platform_data = add;
-
-    client = i2c_new_device(adapter, &info);
-    if (client == NULL) {
-        printk("i2c_new_device failed\n");
-        return -ENODEV;
-    }
     i2c_set_clientdata(client, add);
 
     ret = device_create_file(&client->dev, &dev_attr_x);
@@ -138,6 +118,9 @@ static __devinit int accel_dev_probe(struct i2c_client *client,
     ret = device_create_file(&client->dev, &dev_attr_z);
     if (ret < 0)
         printk("device_create_file z FAILED!\n");
+    ret = device_create_file(&client->dev, &dev_attr_rate);
+    if (ret < 0)
+        printk("device_create_file rate FAILED!\n");
 
     // Do hardware init
     return accel_init(client, add);
@@ -150,6 +133,10 @@ static __devexit int accel_dev_remove(struct i2c_client * client)
 
     // Deinit hardware
     accel_deinit(client);
+    device_remove_file(client->dev, &dev_attr_x);
+    device_remove_file(client->dev, &dev_attr_y);
+    device_remove_file(client->dev, &dev_attr_z);
+    device_remove_file(client->dev, &dev_attr_rate);
     kfree(add);
     return 0;
 }
@@ -172,7 +159,27 @@ static struct i2c_driver accel_dev_driver = {
 
 static int __init accel_dev_init(void)
 {
+    struct i2c_adapter    * adapter;
+    struct i2c_client     * client;
+    struct i2c_board_info   info = {};
+
     printk("accel_dev_init\n");
+    adapter = i2c_get_adapter(1);
+    if (adapter == NULL) {
+        printk("i2c_get_adaptter failed\n");
+        return -ENODEV;
+    }
+
+    strlcpy(info.type, "accelerometer", I2C_NAME_SIZE);
+    info.addr = LIS331DLH_I2C_ADDR;
+    info.platform_data = add;
+
+    client = i2c_new_device(adapter, &info);
+    if (client == NULL) {
+        printk("i2c_new_device failed\n");
+        return -ENODEV;
+    }
+
     return i2c_add_driver(&accel_dev_driver);
 }
 
