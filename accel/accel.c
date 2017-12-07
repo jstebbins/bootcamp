71646a914e342f97151c9b7ea99d4875206fd117
John Stebbins <stebbins@jetheaddev.com>
2017-12-07 11:30:47 -0800

    Fix compile errors

--- accel/accel.c
+++ accel/accel.c
@@ -9,6 +9,7 @@
 #include <linux/fs.h>
 #include <linux/uaccess.h>
 #include <linux/miscdevice.h>
+#include <linux/i2c.h>
 #include "lis331dlh_regs.h"
 
 #define DRV_VERSION "0.01"
@@ -20,9 +21,6 @@ struct accel_device_data {
 static ssize_t accel_x_show(struct device *dev,
                             struct device_attribute *attr, char *buf)
 {
-    u16 value;
-    int loop;
-
     snprintf(buf, PAGE_SIZE, "TODO");
     return strlen(buf) + 1;
 }
@@ -30,9 +28,6 @@ static ssize_t accel_x_show(struct device *dev,
 static ssize_t accel_y_show(struct device *dev,
                             struct device_attribute *attr, char *buf)
 {
-    u16 value;
-    int loop;
-
     snprintf(buf, PAGE_SIZE, "TODO");
     return strlen(buf) + 1;
 }
@@ -40,9 +35,6 @@ static ssize_t accel_y_show(struct device *dev,
 static ssize_t accel_z_show(struct device *dev,
                             struct device_attribute *attr, char *buf)
 {
-    u16 value;
-    int loop;
-
     snprintf(buf, PAGE_SIZE, "TODO");
     return strlen(buf) + 1;
 }
@@ -50,9 +42,6 @@ static ssize_t accel_z_show(struct device *dev,
 static ssize_t accel_rate_show(struct device *dev,
                                struct device_attribute *attr, char *buf)
 {
-    u16 value;
-    int loop;
-
     snprintf(buf, PAGE_SIZE, "TODO");
     return strlen(buf) + 1;
 }
@@ -80,7 +69,6 @@ static DEVICE_ATTR(rate, 0644, accel_rate_show, accel_rate_store);
 static int accel_init(struct i2c_client * client,
                       struct accel_device_data *add)
 {
-    int ret;
     u32 who_am_i;
 
     who_am_i = i2c_smbus_read_byte_data(client, LIS331DLH_WHO_AM_I_REG);
@@ -132,11 +120,11 @@ static __devexit int accel_dev_remove(struct i2c_client * client)
     printk("accel_dev_remove\n");
 
     // Deinit hardware
-    accel_deinit(client);
-    device_remove_file(client->dev, &dev_attr_x);
-    device_remove_file(client->dev, &dev_attr_y);
-    device_remove_file(client->dev, &dev_attr_z);
-    device_remove_file(client->dev, &dev_attr_rate);
+    accel_deinit(client, add);
+    device_remove_file(&client->dev, &dev_attr_x);
+    device_remove_file(&client->dev, &dev_attr_y);
+    device_remove_file(&client->dev, &dev_attr_z);
+    device_remove_file(&client->dev, &dev_attr_rate);
     kfree(add);
     return 0;
 }
@@ -149,7 +137,7 @@ MODULE_DEVICE_TABLE(i2c, accel_id);
 
 static struct i2c_driver accel_dev_driver = {
     .driver = {
-        .name = "accelerometer";
+        .name = "accelerometer",
         .owner = THIS_MODULE,
     },
     .id_table = accel_id,
@@ -172,7 +160,6 @@ static int __init accel_dev_init(void)
 
     strlcpy(info.type, "accelerometer", I2C_NAME_SIZE);
     info.addr = LIS331DLH_I2C_ADDR;
-    info.platform_data = add;
 
     client = i2c_new_device(adapter, &info);
     if (client == NULL) {
